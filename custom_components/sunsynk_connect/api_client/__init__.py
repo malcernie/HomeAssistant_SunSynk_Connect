acf3f004a1b26ad45a5115cc600f78c1c6a1338d
malcernie <stuart@malcernie.com>
2025-03-14 13:47:52 +0000

    Update __init__.py

--- custom_components/sunsynk_connect/api_client/__init__.py
+++ custom_components/sunsynk_connect/api_client/__init__.py
@@ -1,25 +1,36 @@
-class OctopusEnergyApiClient:
+import logging
+import json
+import aiohttp
+from asyncio import TimeoutError
+from datetime import (datetime, timedelta, time, timezone)
+from threading import RLock
+
+from homeassistant.util.dt import (as_utc, now, as_local, parse_datetime, parse_date)
+
+_LOGGER = logging.getLogger(__name__)
+
+user_agent_value = "malcernie-ha-sunsynk-connect"
+
+class SunsynkConnectApiClient:
   _refresh_token_lock = RLock()
   _session_lock = RLock()
 
-  def __init__(self, api_key, electricity_price_cap = None, gas_price_cap = None, timeout_in_seconds = 20, favour_direct_debit_rates = True):
-    if (api_key is None):
-      raise Exception('API KEY is not set')
+  def __init__(self, username, password, timeout_in_seconds = 20):
+    if (username is None):
+      raise Exception('username is not set')
+    if (password is None):
+      raise Exception('password is not set')
 
-    self._api_key = api_key
-    self._base_url = 'https://api.octopus.energy'
+    self._username = username
+    self._password = password
+    self._oauth_url = 'https://api.sunsynk.net/oauth/token'
+    self._base_url = 'https://api.sunsynk.net/api/v1'
 
-    self._graphql_token = None
-    self._graphql_expiration = None
-
-    self._product_tracker_cache = dict()
-
-    self._electricity_price_cap = electricity_price_cap
-    self._gas_price_cap = gas_price_cap
-    self._favour_direct_debit_rates = favour_direct_debit_rates
+    self._api_token = None
+    self._api_token_expiration = None
 
     self._timeout = aiohttp.ClientTimeout(total=None, sock_connect=timeout_in_seconds, sock_read=timeout_in_seconds)
-    self._default_headers = { "user-agent": f'{user_agent_value}/{INTEGRATION_VERSION}' }
+    self._default_headers = { "user-agent": f'{user_agent_value}' }
 
     self._session = None
 
@@ -41,31 +52,63 @@ class OctopusEnergyApiClient:
 
   async def async_refresh_token(self):
     """Get the user's refresh token"""
-    if (self._graphql_expiration is not None and (self._graphql_expiration - timedelta(minutes=5)) > now()):
+    if (self._api_token_expiration is not None and (self._api_token_expiration - timedelta(minutes=5)) > now()):
       return
 
     with self._refresh_token_lock:
       # Check that our token wasn't refreshed while waiting for the lock
-      if (self._graphql_expiration is not None and (self._graphql_expiration - timedelta(minutes=5)) > now()):
+      if (self._api_token_expiration is not None and (self._api_token_expiration - timedelta(minutes=5)) > now()):
         return
 
       try:
-        client = self._create_client_session()
-        url = f'{self._base_url}/v1/graphql/'
-        payload = { "query": api_token_query.format(api_key=self._api_key) }
+      client = self._create_client_session()
+        url = f'{self._oauth_url}'
+        payload = { "username": self._username, "password", "password": self._password, "grant_type": "password", client_id: "openapi"  }
         headers = { integration_context_header: "refresh-token" }
-        async with client.post(url, headers=headers, json=payload) as token_response:
-          token_response_body = await self.__async_read_response__(token_response, url)
-          if (token_response_body is not None and 
-              "data" in token_response_body and
-              "obtainKrakenToken" in token_response_body["data"] and 
-              token_response_body["data"]["obtainKrakenToken"] is not None and
-              "token" in token_response_body["data"]["obtainKrakenToken"]):
+        async with client.post(url, headers=headers, json=payload) as client_response:
+          client_response_body = await self.__async_read_response__(client_response, url)
+          _LOGGER.debug(f'inverters: {client_response_body}')
+          
+          if (client_response_body is not None and 
+              "msg" in client_response_body and
+              client_response_body["msg"].lower() == 'success' and
+              "data" in client_response_body and
+              "access_token" in client_response_body["data"] and 
+              client_response_body["data"]["access_token"] is not None):
             
-            self._graphql_token = token_response_body["data"]["obtainKrakenToken"]["token"]
-            self._graphql_expiration = now() + timedelta(hours=1)
+            self._api_token = client_response_body["data"]["token"]
+            self._api_token_expiration = now() + timedelta(hours=1)
           else:
             _LOGGER.error("Failed to retrieve auth token")
       except TimeoutError:
         _LOGGER.warning(f'Failed to connect. Timeout of {self._timeout} exceeded.')
         raise TimeoutException()
+
+  async def async_list_inverters(self, account_id):
+    """List inverters"""
+    await self.async_refresh_token()
+
+    try:
+      client = self._create_client_session()
+      url = f'{self._base_url}/inverters'
+      payload = { }
+      headers = { "Authorization": f"BEARER {self._api_token}" }
+      async with client.get(url, json=payload, headers=headers) as client_response:
+        client_response_body = await self.__async_read_response__(client_response, url)
+        _LOGGER.debug(f'inverters: {client_response_body}')
+
+        if (client_response_body is not None and 
+            "msg" in client_response_body and
+            client_response_body["msg"].lower() == 'success' and
+            "data" in client_response_body and 
+            "infos" in client_response_body["data"] and 
+            client_response_body["data"]["infos"] is not None):
+          return client_response_body["data"]["infos"]
+        else:
+          _LOGGER.error("Failed to retrieve account")
+    
+    except TimeoutError:
+      _LOGGER.warning(f'Failed to connect. Timeout of {self._timeout} exceeded.')
+      raise TimeoutException()
+    
+    return None
